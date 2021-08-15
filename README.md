4293a7ae6defcc9c0f1c48d0844d06fa174e69c2
A_Prabowo <agungprabowo8800@gmail.com>
2021-08-15 21:55:13 +0700

    folder structur and flask server

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# backend-API
+# waste-API
+
+## About
---
+++ app/model/users.py
@@ -0,0 +1,12 @@
+from app import db 
+from datetime import datetime
+
+class Users(db.Model):
+    id_user = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(250), nullable=False)
+    username = db.Column(db.String(50), index=True, unique=True, nullable=False)
+    password = db.Column(db.String(50), nullable=False)
+    role = db.Column(db.Enum('Admin', 'User'), nullable=False)
+
+    def __repr__(self):
+        return '<Users {}>'.format(self.name)
