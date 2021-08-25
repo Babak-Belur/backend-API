7c578e3f224d51a0de42df12e9181db514a5f438
A_Prabowo <agungprabowo8800@gmail.com>
2021-08-25 13:52:27 +0700

    success adding predict grade url

--- app/controller/ForecastController.py
+++ app/controller/ForecastController.py
@@ -1,3 +1,4 @@
+import json
 from app.model.users import Users
 from app.model.target import Target
 from app.model.evaluation import Evaluation
@@ -10,45 +11,45 @@ from sqlalchemy import text
 import tensorflow as tf
 from keras.models import load_model
 
+
 model = load_model('C:/Users/hp/Documents/backend-API/GF_V2_model.h5')
 
 def prediction(id):
-    #connection = db.session.connection()
-    #sql = text("SELECT * FROM evaluation e inner join target t on t.id_target = e.id_target inner join users u on u.id_user = e.id_user inner join course_subject c on c.id_course = t.id_course where id_evaluation = :evaluation").params(evaluation=id)
-    #result = db.engine.execute(sql)
-    #names = [row[0] for row in result]
-    #print names
-
     evaluation = db.engine.execute(
             text("SELECT e.study_time, e.freetime, t.g1, e.grade FROM evaluation e inner join target t on t.id_target = e.id_target inner join users u on u.id_user = e.id_user inner join course_subject c on c.id_course = t.id_course where id_evaluation = :evaluation").params(evaluation=id)
     )
 
-    studyTime = evaluation.study_time / 4
-    freeTime = evaluation.freetime / 5
-    #studyTime = 3 / 4
-    #freeTime = 3 / 5
-
-    #G1 = 90 / 100
-    #G2 = 90 / 100
-    G1 = evaluation.g1 / 100
-    G2 = evaluation.grade / 100
 
     data = formatArray(evaluation)
-    #data = {
-    #        'study_time': evaluation.study_time,
-    #        'freetime': evaluation.freetime,
-    #        #'g1': data.g1,
-    #        'grade': evaluation.grade
-    #}
 
-    #input_data = [[studyTime, freeTime, G1, G2]]
+    studyTime = int(data[0]['study_time']) / 4
+    freeTime = int(data[0]['freetime']) / 5
+    G1 = int(data[0]['g1']) / 100
+    G2 = int(data[0]['grade']) / 100
+    
+    input_data = [[studyTime, freeTime, G1, G2]]
 
-    #prediction = model.predict(input_data)
+    prediction = model.predict(input_data)
 
-    #data = jsonify(prediction)
+    lists = prediction.tolist()
 
+    flatList = [ item for elem in prediction for item in elem]
 
-    return response.success(data, "sukses")
+    strings = [str(integer) for integer in flatList]
+    a_string = "".join(strings)
+    an_integer = float(a_string)
+
+    datas = [
+        {
+            'study_time': data[0]['study_time'],
+            'freetime': data[0]['freetime'],
+            'g1': data[0]['g1'],
+            'grade': data[0]['grade'],
+            'predict_grade': an_integer
+        }
+    ]
+
+    return response.success(datas, "sukses")
 
 
 def formatArray(datas):
@@ -64,7 +65,7 @@ def singleObjectEvaluation(data):
     data = {
             'study_time': data.study_time,
             'freetime': data.freetime,
-            #'g1': data.g1,
+            'g1': data.g1,
             'grade': data.grade
     }
 
